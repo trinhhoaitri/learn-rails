a10edeec4c01ccb62151a04b864f1551d7de37d9
trinhhoaitri <trinhhoaitri@gmail.com>
2014-10-11 18:51:12 +0700

    add configuration

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -27,6 +27,20 @@ LearnRails::Application.configure do
   # number of complex assets.
   config.assets.debug = true
 
+  # learn-rails
+  config.action_mailer.smtp_settings = {
+    address: "smtp.gmail.com",
+    port: 587,
+    domain: ENV["DOMAIN_NAME"],
+    authentication: "plain",
+    enable_starttls_auto: true,
+    user_name: ENV["GMAIL_USERNAME"],
+    password: ENV["GMAIL_PASSWORD"]
+  }
+
+  # Send email in development mode
+  config.action_mailer.perform_deliveries = true
+
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
 end
